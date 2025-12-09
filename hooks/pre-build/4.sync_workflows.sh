b361201a3ee291ac666fac70d945157ba46981c3
LIghtJUNction <lightjunction.me@gmail.com>
2025-12-09 21:30:08 +0800

    fix/pre-build/4

--- hooks/pre-build/4.sync_workflows.sh
+++ hooks/pre-build/4.sync_workflows.sh
@@ -12,12 +12,12 @@ fi
 git submodule update
 
 # cp workflows
-cp -rf "$KAM_PROJECT_ROOT/KamModuleX/.github/workflows" "$KAM_PROJECT_ROOT/.github/workflows"
+cp -rf "$KAM_PROJECT_ROOT/KamModuleX/.github/workflows" "$KAM_PROJECT_ROOT/.github/"
 log_info "Synchronized GitHub workflows from KamModuleX to main project."
 
 # cp to tmpl/*/.github/workflows
 for tmpl_dir in "$KAM_PROJECT_ROOT/tmpl"/*/; do
-	cp -rf "$KAM_PROJECT_ROOT/.github/workflows" "$tmpl_dir/.github/workflows"
+	cp -rf "$KAM_PROJECT_ROOT/.github/workflows" "$tmpl_dir/.github/"
 	log_info "Synchronized GitHub workflows to template: $tmpl_dir"
 done
 
