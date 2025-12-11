1659551836eb6a0a643b5ae05ad88f7b899983f7
LIghtJUNction <lightjunction.me@gmail.com>
2025-12-11 22:03:28 +0800

    remove test_kam

--- test_kam/hooks/post-build/8900.UPDATE_CHANGE_LOG.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. "$KAM_HOOKS_ROOT/lib/utils.sh"
-
-# optionally update changelog using commitizen.
-require_command cz "commitizen not found; cannot update changelog." || exit 0
-
-cz ch
-
--- test_kam/src/test_kam/README.md
+++
@@ -1,5 +0,0 @@
-# test_kam
-
-## lib/nga-utils.sh
-[DOCS](https://app.niggergo.work/docs/nga/shell/)
- 
