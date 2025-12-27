6851baa72bf64bcd3bfa06d560282eec804fa14d
LIghtJUNction <lightjunction.me@gmail.com>
2025-12-27 22:19:28 +0800

    auto/sync

--- hooks/pre-build/0500.CHECK_NDK.sh
+++ hooks/pre-build/0500.CHECK_NDK.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # shellcheck source=../lib/utils.sh
+# shellcheck source=hooks/lib/utils.sh
 . "$KAM_HOOKS_ROOT/lib/utils.sh"
 # Optional: enable debug tracing if requested
 if [ "${KAM_DEBUG:-0}" = "1" ]; then
